f74afa95beb2726e72c085be9cab1ffa34ccf4dd
Jared Burrows <jaredsburrows@gmail.com>
2015-08-01 12:30:15 -0400

    New

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
+#include <vector>
 
 #include <boost/filesystem.hpp>
 
 int main(void) {
 
+    std::vector<std::string> temp = {"test1", "test2"};
+
     std::cout << "Hello" << std::endl;
-    boost::filesystem::path full_path( boost::filesystem::current_path() );
+
+    boost::filesystem::path full_path(boost::filesystem::current_path());
     std::cout << "Current path is : " << full_path << std::endl;
 
     return 0;
